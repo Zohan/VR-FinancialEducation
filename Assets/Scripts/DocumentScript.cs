3adc0c6f8faf0283a8841f86700ef901d761b7c0
Shuang52 <sh@Ss-iMac.local>
2017-09-10 21:22:57 -0700

    Module 4

--- Assets/Scripts/DocumentScript.cs
+++ Assets/Scripts/DocumentScript.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class DocumentScript : MonoBehaviour {
 
+	Vector3 move;
+
 	public void selectdoc(){
 		int yRotation = 90;
 		this.gameObject.transform.Rotate (60, 0, 0);
-		Vector3 move = new Vector3 (139.2f, 184f, 94.1f);
+		switch (gameObject.name) {
+		case "birthcertificate":
+			move = new Vector3 (139.4f, 184f, 94.1f);
+			break;
+		case "marriagecertificate":
+			move = new Vector3 (139.2f, 184.2f, 94.1f);
+			break;
+		case "benefit":
+			move = new Vector3 (138, 183.5f, 94);
+			break;
+		case "socialsecurity (1)":
+			this.gameObject.transform.Rotate (-60, 0, 45);
+			move = new Vector3 (138.7f, 184.5f, 94.2f);
+			break;
+		case "bankstatement":
+			move = new Vector3 (137.5f, 183.5f, 94.2f);
+			break;
+		}
 		gameObject.transform.position = move;
+
 	}
 
 	public void gazedoc(){
