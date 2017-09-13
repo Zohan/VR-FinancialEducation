dfcf25328c1cbaa6fa6a994a0e4d6ca90afb60d5
Shuang52 <sh@Ss-iMac.local>
2017-09-12 22:46:56 -0700

    Module 4

---
+++ Assets/Mod4_AnimationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mod4_AnimationScript : MonoBehaviour {
+
+	Animator animator;
+	int counter;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nextStep(){
+		counter++;
+		animator.SetInteger ("next_step", counter);
+	}
+}
